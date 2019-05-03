cfd53d643ce78afee3eb0c03caecf99561445505
nico <n.tiesenhausen@gmail.com>
2019-05-03 17:48:41 +0200

    travis 1.3.2

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,4 @@
 
 sshpass -p Jawaqo65 ssh -o StrictHostKeyChecking=no blue_turtle@vps554131.ovh.net bash "cd /home/blue_turtle/devops
 && git pull origin develop";
-echo "fini"
+
---
+++ deploy_master.sh
@@ -0,0 +1,2 @@
+sshpass -p Jawaqo65 ssh -o StrictHostKeyChecking=no blue_turtle@vps554131.ovh.net bash "cd /home/blue_turtle/devops_master
+&& git pull origin master";
