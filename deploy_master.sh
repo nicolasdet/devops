3901e204515cf55cfa5470d75e9f82d66f74be24
nico <n.tiesenhausen@gmail.com>
2019-05-03 18:00:29 +0200

    commit deploy last 1.0

--- deploy_master.sh
+++ deploy_master.sh
@@ -1,2 +1,2 @@
 sshpass -p Jawaqo65 ssh -o StrictHostKeyChecking=no blue_turtle@vps554131.ovh.net bash "cd /home/blue_turtle/devops_master
-&& git pull origin master";
+&& git reset --hard origin/master && docker run -d -p 80:80 cn1991/machinezend:prod";
